0e9a77a04381ea6d3701e4a6bb80bde6c15e145d
Nils Petzall <nils.petzall@gmail.com>
2024-01-14 18:14:32 +0100

    chore: remove GetExistingUrls and Classpath interface

--- src/main/java/org/schemaspy/input/dbms/classloader/ClClasspath.java
+++ src/main/java/org/schemaspy/input/dbms/classloader/ClClasspath.java
@@ -1,42 +1,53 @@
 package org.schemaspy.input.dbms.classloader;
 
-import org.schemaspy.input.dbms.classpath.Classpath;
-
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLClassLoader;
-import java.util.List;
+import java.nio.file.Path;
 import java.util.Objects;
-import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+
+import org.schemaspy.input.dbms.driverpath.Driverpath;
+import org.schemaspy.input.dbms.driverpath.IterableFilter;
+import org.schemaspy.input.dbms.driverpath.IterableMap;
 
 /**
  * Encapsulates how to access the classloader specified by a classpath.
  */
 public final class ClClasspath implements ClassloaderSource {
 
-    private final Classpath classpath;
+    private final Iterable<URL> classPath;
 
-    public ClClasspath(final Classpath classpath) {
-        this.classpath = classpath;
+    public ClClasspath(final Driverpath driverpath) {
+        this.classPath = new IterableFilter<>(
+            new IterableMap<>(
+                new IterableMap<>(
+                    driverpath, Path::toUri
+                ), uri -> {
+                try {
+                    return uri.toURL();
+                } catch (MalformedURLException e) {
+                    return null;
+                }
+            }
+            ),
+            Objects::nonNull
+        );
     }
 
     @Override
     public ClassLoader classloader() {
+        final URL[] urls = StreamSupport
+            .stream(classPath.spliterator(), false)
+            .toArray(URL[]::new);
+
         // if a classpath has been specified then use it to find the driver,
         // otherwise use whatever was used to load this class.
         // thanks to Bruno Leonardo Gonalves for this implementation that he
         // used to resolve issues when running under Maven
 
-        final List<URL> urls = this.classpath.paths().stream().map(uri -> {
-            try {
-                return uri.toURL();
-            } catch (MalformedURLException e) {
-                return null;
-            }
-        }).filter(Objects::nonNull).collect(Collectors.toList());
-
         return new URLClassLoader(
-                urls.toArray(new URL[urls.size()]),
+                urls,
                 new ClDefault().classloader()
         );
     }
